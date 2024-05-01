0309e4b69faba22319c54990525d5e68b2dd112e
Jeremy Samuel <jeremysamuel@Jeremys-MacBook-Air.local>
2024-05-01 16:02:57 -0400

    new updates

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-const express = require('express');
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+//import authRoute from "./api/routes/auth.js"
+
 const app = express();
+dotenv.config()
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGOURL);
+    console.log("Connected to mongoDB");
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconnected!");
+});
+
+// middlewares
+//app.use("/api/auth", authRoute)
 
 app.listen(4000, ()=> {
-    console.log(`Connected to backed on port 4000`)
+    connect();
+    console.log("Connected to backend")
 })
